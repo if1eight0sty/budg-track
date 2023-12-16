a3b4fc8b3933624d1d391813d3e2388844a7f311
A18yan <ma18yan@gmail.com>
2023-12-16 21:34:25 +0545

    Update header styling and home page content

--- budg-track/src/layout/header/header.jsx
+++ budg-track/src/layout/header/header.jsx
@@ -1,15 +1,14 @@
-// import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header className="py-2 px-2 text-[#2e2e2e]/80 @[30em]:px-6 @[50em]:px-10 @[1300px]:px-16 @[1300px]:py-3 border-b sticky top-0 z-10">
+      <header className="py-2 px-2 text-[#2e2e2e]/80 @[30em]:px-6 @[50em]:px-10 @[1300px]:px-16 @[1300px]:py-3 border-b sticky top-0 z-10 bg-white">
         <nav className="flex justify-between items-center">
           <span className="cursor-pointer select-none" title="BudgTrack">
-            <Icon icon="mingcute:pig-money-fill" width={40} />
+            <Icon icon="mingcute:pig-money-fill" width={35} />
           </span>
-          <ul className="flex gap-x-3 font-semibold uppercase">
+          <ul className="flex gap-x-3 font-semibold uppercase text-[.95rem]">
             <li>
               <Link to="/">Dashboard</Link>
             </li>
@@ -19,7 +18,7 @@ const Header = () => {
             className="border rounded-full cursor-pointer select-none border-[#2e2e2e] p-[3px]"
             title="View profile"
           >
-            <Icon icon="memory:user" width={30} />
+            <Icon icon="memory:user" width={20} />
           </span>
         </nav>
       </header>
--- budg-track/src/pages/home/home.jsx
+++ budg-track/src/pages/home/home.jsx
@@ -1,5 +1,11 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="@[30em]:px-6 @[50em]:px-10 @[1300px]:px-16 bg-slate-100">
+        home
+      </div>
+    </>
+  );
 };
 
 export default Home;
