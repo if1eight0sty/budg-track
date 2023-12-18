f99138cd96ac9ebb566f104b373fff7cd0d5fee1
A18yan <ma18yan@gmail.com>
2023-12-18 12:49:19 +0545

    Add lazy loading for Home component

--- bidget_track/src/App.tsx
+++ bidget_track/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
+const Home = lazy(() => import("./pages/home/home"));
 const Layout = lazy(() => import("./layout/layout"));
 const Register = lazy(() => import("./pages/auth/register"));
 const Login = lazy(() => import("./pages/auth/login"));
@@ -12,8 +13,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Layout />}>
-            {/* <Route path="" element={<Home />} />
-          <Route path="add-budget" element={<AddBudget />} /> */}
+            <Route path="" element={<Home />} />
+            {/*  <Route path="add-budget" element={<AddBudget />} /> */}
           </Route>
         </Routes>
       </Suspense>
---
+++ bidget_track/src/pages/home/search/search.tsx
@@ -0,0 +1,14 @@
+import SearchField from "./components/search-field";
+
+const Search = () => {
+  return (
+    <>
+      <section className="w-full">
+        <SearchField />
+        {/* <FilterFields /> */}
+      </section>
+    </>
+  );
+};
+
+export default Search;
