5198918b4e9b436576de500e64471a81f526fd20
A18yan <ma18yan@gmail.com>
2023-12-18 11:41:20 +0545

    Remove unused helper and layout files

--- bidget_track/src/layout/helper.ts
+++ bidget_track/src/layout/helper.ts
@@ -4,7 +4,7 @@ export class CheckAuthClass {
   // The checkAuth method checks if a user is authenticated by retrieving the user object from local storage.
   // If the user object is not found, it returns false. Otherwise, it returns true.
   checkAuth = () => {
-    const user = JSON.parse(localStorage.getItem("user"));
+    const user = JSON.parse(localStorage.getItem("user") || "");
     if (!user) {
       return false;
     }
