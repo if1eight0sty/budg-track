0c09f3d3bd2522ba2cf43c006007926f0ca79889
A18yan <ma18yan@gmail.com>
2023-12-18 12:32:19 +0545

    Add lazy loading and routing to App component

--- bidget_track/src/App.tsx
+++ bidget_track/src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+const Layout = lazy(() => import("./layout/layout"));
+const Register = lazy(() => import("./pages/auth/register"));
+const Login = lazy(() => import("./pages/auth/login"));
+const Loader = lazy(() => import("./components/loader"));
 function App() {
-  return <></>;
+  return (
+    <>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Layout />}>
+            {/* <Route path="" element={<Home />} />
+          <Route path="add-budget" element={<AddBudget />} /> */}
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
